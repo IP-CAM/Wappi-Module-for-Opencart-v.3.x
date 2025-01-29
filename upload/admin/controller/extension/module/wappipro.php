8466409c26c629fd20815b8c7383b4d95c630d74
debcomedian <vital.egorov2004@gmail.com>
2025-01-29 12:36:29 +0300

    v1.2.1

--- upload/admin/controller/extension/module/wappipro.php
+++ upload/admin/controller/extension/module/wappipro.php
@@ -22,6 +22,12 @@ class ControllerExtensionModuleWappiPro extends Controller
         "wappipro_apiKey"   => ["label" => "API Key", "type" => "isEmpty", "value" => "", "validate" => true],
     ];
     
+    public function __construct($registry) {
+        parent::__construct($registry);
+        $this->load->library('wappiproreplacements');
+        $this->replacementsHandler = new WappiProReplacements($registry);
+    }
+
 
     public function index()
     {
@@ -74,8 +80,6 @@ class ControllerExtensionModuleWappiPro extends Controller
 
         $data['btn_token_save_all'] = $this->language->get('btn_token_save_all');
 
-        $data['btn_status_order_description'] = $this->language->get('btn_status_order_description');
-
         $data['order_status_list']    = $this->model_localisation_order_status->getOrderStatuses();  // ??
         
         $data['wappipro_test_result'] = $this->testResult;
@@ -84,6 +88,11 @@ class ControllerExtensionModuleWappiPro extends Controller
         $data['wappipro_order_status_active'] = [];
         $data['wappipro_order_status_message'] = [];
         $data['wappipro_admin_order_status_active'] = [];
+
+        $data['text_available_variables'] = $this->language->get('text_available_variables');
+        $data['text_variable'] = $this->language->get('text_variable');
+        $data['text_description'] = $this->language->get('text_description');
+        $data['text_close'] = $this->language->get('text_close');
 
         foreach ($data['order_status_list'] as $status) {
             $data['wappipro_order_status_active'][$status['order_status_id']] = $settings['wappipro_' . $status['order_status_id'] . '_active'] ?? '';
@@ -95,7 +104,40 @@ class ControllerExtensionModuleWappiPro extends Controller
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['footer']      = $this->load->controller('common/footer');
 
+        $lastOrderId = $this->getLastOrderId();
+        if ($lastOrderId) {
+            $this->replacementsHandler->loadReplacements($lastOrderId);
+        
+            $data['replacements'] = $this->replacementsHandler->getReplacements();
+        } else {
+            $data['replacements'] = [];
+            $data['error_warning'][] = ['error' => 'No orders found.'];
+        }
+
         $this->response->setOutput($this->load->view('extension/module/wappipro', $data));
+    }
+
+    public function getLastOrderId() {
+        $query = $this->db->query("SELECT order_id FROM `" . DB_PREFIX . "order` ORDER BY order_id DESC LIMIT 1");
+        if ($query->num_rows > 0) {
+            return (int)$query->row['order_id'];
+        }
+    
+        return null;
+    }
+
+    private function getOrderReplacements($order_id)
+    {
+        $this->load->model('sale/order');
+        $order_info = $this->model_sale_order->getOrder($order_id);   
+        if (!$order_info) {
+            return [];
+        }
+        $replacements = [];
+        foreach ($order_info as $key => $value) {
+            $replacements[$key] = '{' . $key . '}';
+        }
+        return $replacements;
     }
 
     public function isModuleEnabled()
